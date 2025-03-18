b187f1b5e83e8a2cbffd2d0c8f49359012a85804
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-03-18 19:44:45 -0400

    Base breathing page visuals established

--- lib/breathe.dart
+++ lib/breathe.dart
@@ -20,20 +20,55 @@ class BreathePage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             SizedBox(height: 25),
+            
+            /* Heart Message */
             Container(
               height: 40, 
               width: 420,
               decoration: BoxDecoration(
-                color: const Color.fromRGBO(22, 103, 135, 1)
+                color: const Color.fromRGBO(22, 135, 99, 1)
               ),
               child: Align(
                 child: Text(
-                  "Just a reminder that...",
-                  //style: widget.theme.textTheme.titleSmall,
+                  "Copy the heart's timing..",
+                  style: AppTheme.theme.textTheme.titleSmall,
                 ),
               ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 60),
+
+            /* Heart Animation */
+            Container(
+              height: 400,
+              child: Image.asset("assets/images/heart_pump.png"),
+            ),
+            SizedBox(height: 20),
+
+            /* Instructions Box */
+            Container(
+              height: 200, 
+              width: 380, 
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(159, 236, 149, 1),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey, 
+                    offset: const Offset(2.0, 3.0),
+                    blurRadius: 5.0, 
+                  ),
+                ],
+              ),
+              child: Padding( padding: const EdgeInsets.all(30.0),
+                child: Align(
+                  child: Text(
+                    "Inhale for 4 seconds\nHold for 7 seconds\nExhale for 8 seconds\nRepeat as needed...",
+                    style: AppTheme.theme.textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+
           ],
         ),
       ),
