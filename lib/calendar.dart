ca9453e04ab58819765767c0446cbbb90acebf53
aahmad8727 <aaminaahmad8155@gmail.com>
2025-03-20 00:19:16 -0400

    Calender screen created

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,39 +1,69 @@
 import 'package:flutter/material.dart';
-import 'activity.dart';
+import 'package:table_calender/calendar.dart';
 
-class CalendarView extends StatelessWidget {
-  const CalendarView({super.key});
+class CalendarScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-      child: const CalendarPage(title: 'Reflection Section'),
-     ),
-    );
-  }
+  _CalendarScreenState createState() => _CalendarScreenState();
 }
 
-class CalendarPage extends StatefulWidget {
-  const CalendarPage({super.key, required this.title});
-  final String title;
-  @override
-  State<CalendarPage> createState() => _CalendarPage();
-}
+class _CalendarScreenState extends State<CalendarScreen> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
-class _CalendarPage extends State<CalendarPage> {
+  // Map<DateTime, MoodEntry> moodEntries;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Mood Tracker Calendar'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Calendar View here"),
-          ],
-        ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2020, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // update
+              });
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            // mark days with mood entries
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, day, events) {
+                return events.isNotEmpty
+                    ? Positioned(
+                        bottom: 1,
+                        child: Container(
+                          width: 7,
+                          height: 7,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      )
+                    : null;
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Add Today’s Entry"),
+          ),
+        ],
       ),
     );
   }
