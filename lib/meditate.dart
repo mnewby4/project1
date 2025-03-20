7a62e6c554a699fc607fcb9c421c3892e1f67721
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-03-20 00:34:44 -0400

    Added audio player to meditation screen

--- lib/meditate.dart
+++ lib/meditate.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'theme.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 
 class MyPainter extends CustomPainter {
  final double animationValue;
@@ -87,6 +90,149 @@ class _RippleState extends State<_RippleAnimation> with SingleTickerProviderStat
   }
 }
 
+class _AudioPlayerWidget extends StatefulWidget {
+  const _AudioPlayerWidget({super.key});
+
+  @override
+  State<_AudioPlayerWidget> createState() => _AudioPlayerState();
+}
+
+class _AudioPlayerState extends State<_AudioPlayerWidget> {
+  late AudioPlayer _audioPlayer; 
+
+  Stream<PositionData> get _positionDataStream =>
+    Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
+      _audioPlayer.positionStream, 
+      _audioPlayer.bufferedPositionStream, 
+      _audioPlayer.durationStream, 
+      (position, bufferedPosition, duration) => PositionData(
+        position, 
+        bufferedPosition, 
+        duration ?? Duration.zero,
+      ),
+    );
+
+  @override 
+  void initState() {
+    super.initState(); 
+    _audioPlayer = AudioPlayer()..setAsset("assets/audio/guided.mp3");
+
+    
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200, 
+      width: 380, 
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(135, 202, 229, 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey, 
+            offset: const Offset(2.0, 3.0),
+            blurRadius: 5.0, 
+          ),
+        ],
+      ),
+      child: Padding( padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment:  MainAxisAlignment.center, 
+          children: [
+            StreamBuilder<PositionData>(
+              stream: _positionDataStream, 
+              builder: (context, snapshot) {
+                final positionData = snapshot.data; 
+                return ProgressBar(
+                  barHeight: 8, 
+                  baseBarColor: Color.fromRGBO(22, 103, 135, 1), 
+                  bufferedBarColor: Colors.grey,
+                  progressBarColor: Color.fromRGBO(255, 241, 205, 1,),
+                  thumbColor: Colors.white, 
+                  timeLabelTextStyle: const TextStyle(
+                    color: Colors.white, 
+                    fontWeight: FontWeight.w600,
+                  ),
+                  progress: positionData?.position ?? Duration.zero, 
+                  buffered: positionData?.bufferedPositionStream ?? Duration.zero, 
+                  total: positionData?.duration ?? Duration.zero, 
+                  onSeek: _audioPlayer.seek, 
+                );
+              },
+            ),
+            Stack(
+              alignment: Alignment.center, 
+              children: [
+                Container(
+                  width: 100, 
+                  height: 100, 
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(22, 135, 99, 1),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Controls(audioPlayer: _audioPlayer),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }  
+}
+
+class PositionData {
+  const PositionData(this.position, this.bufferedPositionStream, this.duration);
+  final Duration position; 
+  final Duration bufferedPositionStream; 
+  final Duration duration; 
+}
+
+class Controls extends StatelessWidget {
+  const Controls({super.key, required this.audioPlayer});
+
+  final AudioPlayer audioPlayer;
+  
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<PlayerState>(
+      stream: audioPlayer.playerStateStream, 
+      builder: (context, snapshot) {
+        final playerState = snapshot.data; 
+        final processingState = playerState?.processingState; 
+        final playing = playerState?.playing; 
+
+        if (!(playing ?? false)) {
+          return IconButton(
+            onPressed: audioPlayer.play, 
+            iconSize: 80, 
+            color: Colors.white, 
+            icon: const Icon(Icons.play_arrow_rounded),
+          );
+        } else if (processingState != ProcessingState.completed) {
+          return IconButton(
+            onPressed: audioPlayer.pause, 
+            iconSize: 80, 
+            color: Colors.white, 
+            icon: const Icon(Icons.pause_rounded),
+          );
+        }
+        return const Icon(
+          Icons.play_arrow_rounded, 
+          size: 80, 
+          color: Colors.white, 
+        );
+      },
+    );
+  }
+}
+
 class MeditatePage extends StatelessWidget {
   const MeditatePage({super.key});
 
@@ -131,31 +277,7 @@ class MeditatePage extends StatelessWidget {
             SizedBox(height: 20),
 
             /* Audio Player */
-            AnimatedContainer(
-              height: 200, 
-              width: 380, 
-              curve: Curves.easeInOut, 
-              duration: Duration(seconds: 4),
-              decoration: BoxDecoration(
-                color: const Color.fromRGBO(135, 202, 229, 1),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey, 
-                    offset: const Offset(2.0, 3.0),
-                    blurRadius: 5.0, 
-                  ),
-                ],
-              ),
-              child: Padding( padding: const EdgeInsets.all(30.0),
-                child: Align(
-                  child: Text(
-                    "Add player here",
-                    style: AppTheme.theme.textTheme.bodyMedium,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-            ),
+            _AudioPlayerWidget(),
           ],
         ),
       ),
